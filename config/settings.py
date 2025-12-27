f41439b5b94c5d847749f7d61e8ab8dc1328bc11
hkmtakgn <hikmetakgun94@gmail.com>
2025-12-27 13:17:07 +0300

    'create-my-static-files'

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "my_static_files",]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
